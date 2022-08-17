30fe97d94bedf9cbee6eef159bcee6ea22256742
wsz82 <wojteksz82@gmail.com>
2022-08-17 16:30:18 +0200

    Update help with license

--- src/srapp/__init__.py
+++ src/srapp/__init__.py
@@ -1,12 +1,7 @@
 # -----------------------------------------------------------
 # Copyright (C) 2022 Wojciech Szpikowski
 # -----------------------------------------------------------
-# Licensed under the terms of GNU GPL 2
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# Licensed under the terms of GNU GPL 3
 # ---------------------------------------------------------------------
 import os
 import subprocess
